268ee93891ce5a75844236601705ca63595cbbca
nourtarek885 <nourtarek885@gmail.com>
2025-12-06 22:52:41 +0200

    Create schemas.py

---
+++ app/modules/events/schemas.py
@@ -0,0 +1,54 @@
+from pydantic import BaseModel, Field, EmailStr
+from datetime import date, time
+from typing import Optional
+
+class CreateEvent(BaseModel):
+        title: str
+        date: date
+        time: time
+        location: str
+        description: str
+
+
+
+
+class OrganizedEventResponse(BaseModel):
+    id: str
+    title: str
+    date: date
+    time: time
+    location: str
+    description: str
+    organizer_id: str
+
+    #3shan el get
+    class Config:
+        orm_mode = True
+        allow_population_by_field_name = True
+
+class InvitedEventResponse(BaseModel):
+    id: str
+    title: str
+    date: date
+    time: time
+    location: str
+    description: str
+    attendee_id: str
+    status: str
+
+    class Config:
+        orm_mode = True
+        allow_population_by_field_name = True
+
+class InvitedUser(BaseModel):
+    event_id:str
+    email: Optional[EmailStr] = None
+    username: Optional[str] = None
+
+    class Config:
+        orm_mode = True
+
+class DeleteEvent(BaseModel):
+    event_id:str
+
+
